515d680397bb01f882d0ca2c8f3083a847efdcf8
Violet Cheng <vcheng@pivotal.io>
2019-11-18 10:32:42 +0800

    fix folder problem

--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 ./har-tools -x /tmp/manga $1
 f=0
-for file in $(ls /tmp/manga/dre-aka-p.papy.co.jp/filesv/sc/contents/)
+imgfolder="/tmp/manga/dre-a*"
+#if [ -d "/tmp/manga/dre-aka-f.papy.co.jp" ]; then
+#  imgfolder="/tmp/manga/dre-aka-f.papy.co.jp"
+#elif [ -d "/tmp/manga/dre-aws-p.papy.co.jp" ]; then
+#  imgfolder="/tmp/manga/dre-aws-p.papy.co.jp"
+#else
+#  imgfolder="/tmp/manga/dre-aws-f.papy.co.jp"
+#fi
+for file in $(ls $imgfolder/filesv/sc/contents/)
 do
   f=$file
 done
 
-rm /tmp/manga/dre-aka-p.papy.co.jp/filesv/sc/contents/$f/6s/0/*_*
-python3 ./main.py /tmp/manga/dre-aka-p.papy.co.jp/filesv/sc/contents/$f/6s/0/ $f
+rm $imgfolder/filesv/sc/contents/$f/6s/0/*_*
+python3 ./main.py $imgfolder/filesv/sc/contents/$f/6s/0/ $f
 zip -qj  1.zip ./result/*
 rm -rf ./result
 rm -rf /tmp/manga
